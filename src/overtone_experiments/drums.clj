30ed3ee4532a3f4c7ddc9d408583b7e95b3041e9
Konstantin Cherkashin <konstantin.cherkashin@symmetrics.de>
2015-01-05 18:25:36 +0200

    more sequensing functions

--- src/overtone_experiments/drums.clj
+++ src/overtone_experiments/drums.clj
@@ -2,14 +2,16 @@
   (:require [overtone.studio.fx :as fx])
   (:use [overtone.live]
         [overtone.inst.sampled-piano]
+        [overtone.samples.piano :only [index-buffer]]
         [overtone.inst.synth]
         [overtone.inst.drum :as d]))
 
 
 (def drums-state (atom {:snare {:amp 5 :crackle-amp 20} :hat {:amp 2 :t 0.7 :low 6000 :hi 2000}}))
 
-(swap! drums-state update-in [:snare] (fn [x] {:amp 15 :crackle-amp 40}))
-(swap! drums-state update-in [:hat] (fn [x] {:amp 1.3 :t 0.6}))
+(swap! drums-state update-in [:snare :amp] (fn [x] 5))
+(swap! drums-state update-in [:hat :amp] (fn [x] 1.1))
+
 
 (fx/fx-reverb)
 (kill fx/fx-reverb)
@@ -17,8 +19,6 @@
 (inst-fx! d/snare fx/fx-reverb)
 
 (fx/fx-echo)
-
-(sampled-piano (note :E4) :level 1.5 :decay 0.5 :sustain 2 :attack 0.01)
 
 (ctl (fx/fx-echo) :delay-time 1.7 :decay-time 0.5 :max-delay 0.1)
 
@@ -28,12 +28,18 @@
 
 (def metro (metronome 70))
 
+(defn metro-fx [m fx instr step]
+  (let [beat (m)]
+    (at (m beat) (if (zero? (mod beat step)) (inst-fx! instr fx)))
+    (at (m (inc beat)) (clear-fx instr))
+    (apply-by (m (inc beat)) metro-fx m fx instr step [])))
+
 (metro :bpm 70)
 
-(defn play-beat [nome sound ctl-ref]
-  (let [beat (nome)]
-    (at (nome beat) (apply sound (ctl-deref ctl-ref drums-state)))
-    (apply-by (nome (+ 4 beat)) play-beat nome sound ctl-ref [])))
+(defn play-beat [m sound step ctl-ref ctl-atom]
+  (let [beat (m)]
+    (at (m beat) (if (zero? (mod beat step)) (apply sound (ctl-deref ctl-ref ctl-atom))))
+    (apply-by (m (inc beat)) play-beat m sound step ctl-ref ctl-atom [])))
 
 (defn play-snare [nome sound ctl-ref]
   (let [beat (nome)]
@@ -42,16 +48,16 @@
 
 (defn ctl-deref [ctl-ref ctl-atom]
   (let [ctl-map (ctl-ref @ctl-atom)]
-    (apply concat (for [key (keys ctl-map)] (list key (get ctl-map key))))))
+    (apply concat (for [key (keys ctl-map)]
+                    (list key (get ctl-map key))))))
 
 (ctl-deref :hat drums-state)
 
-(play-beat metro d/snare :snare)
-(play-snare metro d/open-hat :hat)
+(play-beat metro d/snare 1 :snare drums-state)
+(play-beat metro d/open-hat 2 :hat drums-state)
+(metro-fx metro fx/fx-reverb d/snare 3)
 
 (ctl d/snare :crackle-amp 20 :amp 5)
-
-
 
 (d/snare)
 (metro)
@@ -60,8 +66,6 @@
 
 (inst-fx! d/snare fx/fx-echo)
 (clear-fx d/snare)
-
-
 
 (print (ctl d/snare :crackle-amp 20.0 :amp 5))
 (def my-hat (d/open-hat :amp 2 :t 0.7))
--- src/overtone_experiments/experiments.clj
+++ src/overtone_experiments/experiments.clj
@@ -1,6 +1,7 @@
 (ns overtone-experiments.experiments
   (:require [overtone.studio.fx])
   (:use [overtone.live]
+        [overtone.samples.piano :only [index-buffer]]
         [overtone.inst.sampled-piano]))
 
 (odoc bus-monitor)
@@ -10,9 +11,46 @@
 (bus-monitor )
 
 
+(definst sampled-piano2
+  [note 60 level 1 rate 1 loop? 0
+   attack 0 decay 1 sustain 1 release 0.1 curve -4 gate 1]
+  (let [buf (index:kr (:id index-buffer) note)
+        env (env-gen (adsr attack decay sustain release level curve)
+                     :gate gate
+                     :action FREE)]
+    (* env (scaled-play-buf 2 buf :rate rate :level level :loop loop? :action FREE))))
+
+(sampled-piano (note :E4) :level 1.5 :decay 0.5 :sustain 2 :attack 0.01)w
+
+(sampled-piano2 (note :E4))
+
+(inst-fx! sampled-piano fx/fx-reverb)
+
+(kill sampled-piano)
+
+
 
 (defsynth first-piano [note 66]
-  (out 0 (sampled-piano :note note)))
+  (out 0 (sampled-piano  (:note note))))
+
+(definst pink-noisey []
+     (* (env-gen (perc 0.1 1.8) :action FREE)
+        (pink-noise)))
+
+(pink-noisey)
+
+(definst sawzall [freq 440]
+  (* (env-gen (perc 0.1 0.8) :action FREE)
+     (saw freq)))
+
+(sawzall)
+
+(definst triangular [freq 120]
+   (* (env-gen (perc 0.1 4.8) :action FREE)
+      (lf-tri freq)))
+
+(triangular 320)
+
 
 (mix:ar)
 
